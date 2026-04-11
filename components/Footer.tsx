0ca647bf5ccb5ae013898b1dbe6fef5f78fd6405
24xconstruction <24xconstruction@gmail.com>
2026-04-11 12:45:50 +0930

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
             <div style={{ marginBottom: '1rem', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1rem' }}>
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img 
-                src="https://res.cloudinary.com/dqdssjby3/image/upload/v1775841381/24xConstrunction_on0otm.png"
+                src="https://res.cloudinary.com/dqdssjby3/image/upload/v1775841381/24xConstruction_on0otm.png"
                 alt="24X Construction Logo"
                 style={{ 
                   width: '160px', 
